37cdce084a143f0457d537d6c944caee0040b422
kevinwang <kwangsan@gmail.com>
2021-10-30 20:34:33 -0400

    Brew formula update for tk version v0.0.8

--- Formula/tk.rb
+++ Formula/tk.rb
@@ -5,20 +5,20 @@
 class Tk < Formula
   desc "Example binary distribution using homebrew."
   homepage "https://github.com/thiskevinwang/tk"
-  version "0.0.7"
+  version "0.0.8"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/thiskevinwang/tk/releases/download/v0.0.7/homebrew-go-example_0.0.7_Darwin_x86_64.tar.gz"
-      sha256 "23c38b7aeddca3836796b9b4027516afeea5dda1d97d0366d141063492855c4f"
+      url "https://github.com/thiskevinwang/tk/releases/download/v0.0.8/homebrew-go-example_0.0.8_Darwin_x86_64.tar.gz"
+      sha256 "fcd9953dd959f1318c8e1e60eebd7e5c38411282291892b8f9d3d37a77452eae"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/thiskevinwang/tk/releases/download/v0.0.7/homebrew-go-example_0.0.7_Linux_x86_64.tar.gz"
-      sha256 "14cc3a4d27a22da73b155fde4e4bafa1a25094dbe88b47c6a995b8253f561ee6"
+      url "https://github.com/thiskevinwang/tk/releases/download/v0.0.8/homebrew-go-example_0.0.8_Linux_x86_64.tar.gz"
+      sha256 "ef884a005cd276c8214b005485e11b434d654c383559364162a7e66b99acf739"
     end
   end
 
